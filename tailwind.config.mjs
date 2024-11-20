078029690fcd4bb8d2903c2dc7361b04bd69f08e
muzamil-dev <mshamsifl@gmail.com>
2024-11-20 02:30:47 -0500

    Add helper theme colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,9 +3,19 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors:{
+			'primary': 'colors.ctp-pink',
+			'secondary': 'colors.ctp-mauve',
+			'accent': 'ctp-yellow',
+			'neutral': 'ctp-overlay0',
+			'info': 'ctp-blue',
+			'success': 'ctp-teal',
+			'warning': 'ctp-peach',
+			'error': 'ctp-red'
+		},
 	},
 	plugins: [require("@catppuccin/tailwindcss")({
 		prefix: "ctp",
-		defaultFlavour: "mocha",
+		// defaultFlavour: "mocha",
 	  })],
 }
