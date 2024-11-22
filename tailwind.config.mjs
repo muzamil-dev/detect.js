612f820ef0ae9953ae097b67d4435dab818e9f9c
muzamil-dev <mshamsifl@gmail.com>
2024-11-22 18:04:46 -0500

    Create dynamic theme structure

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,21 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-		colors:{
-			'primary': 'colors.ctp-pink',
-			'secondary': 'colors.ctp-mauve',
-			'accent': 'ctp-yellow',
-			'neutral': 'ctp-overlay0',
-			'info': 'ctp-blue',
-			'success': 'ctp-teal',
-			'warning': 'ctp-peach',
-			'error': 'ctp-red'
-		},
-	},
-	plugins: [require("@catppuccin/tailwindcss")({
-		prefix: "ctp",
-		// defaultFlavour: "mocha",
-	  })],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "var(--color-primary)",
+        "primary-content": "var(--color-primary-content)",
+        secondary: "var(--color-secondary)",
+        "secondary-content": "var(--color-secondary-content)",
+        accent: "var(--color-accent)",
+        "accent-content": "var(--color-accent-content)",
+        neutral: "var(--color-neutral)",
+        "base-100": "var(--color-base-100)",
+        "base-200": "var(--color-base-200)",
+        "base-300": "var(--color-base-300)",
+        info: "var(--color-info)",
+        "info-content": "var(--color-info-content)",
+        success: "var(--color-success)",
+        "success-content": "var(--color-success-content)",
+        warning: "var(--color-warning)",
+        "warning-content": "var(--color-warning-content)",
+        error: "var(--color-error)",
+        "error-content": "var(--color-error-content)",
+        "neutral-content": "var(--color-neutral-content)",
+        "base-content": "var(--color-base-content)",
+      },
+    },
+  },
+  plugins: [
+    require("@catppuccin/tailwindcss")({
+      prefix: "ctp",
+      // defaultFlavour: "mocha",
+    }),
+  ],
+};
