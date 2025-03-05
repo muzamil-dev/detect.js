248971237bef284da4bc1f4aab6be3d40e79546d
muzamil-dev <mshamsifl@gmail.com>
2025-03-05 11:07:37 -0500

    Update session.ts to insert session data

--- src/scripts/session.ts
+++ src/scripts/session.ts
@@ -1,5 +1,6 @@
-<<<<<<< HEAD
-import { writable } from "svelte/store";
+import { writable, get } from "svelte/store";
+import { insertAnalysisData } from "./insert";
+import { analysisData } from "../scripts/websocket";
 
 export const sessionId = writable<number | null>(null); 
 
@@ -35,55 +36,29 @@ export async function createSession(sessionData: {
 
     // Save sessionId to store
     if (result.sessionId) {
-      sessionId.set(result.sessionId); // Store the sessionId in the writable store
-    }
+      sessionId.set(Number(result.sessionId)); // Ensure it's treated as a number
+      uploadData(); // Upload the data to the server
+    }    
 
   } catch (error) {
     console.error("Error creating session:", error);
   }
 }
-=======
-import { writable } from "svelte/store";
 
-export const sessionId = writable<number | null>(null); 
-
-const serverAddress = "http://localhost:8080/createSession";
-
-export async function createSession(sessionData: {
-  name: string;
-  start_time: string;
-  end_time: string;
-  var_min: number;
-  var_max: number;
-  acc_min: number;
-  acc_max: number;
-}) {
-  try {
-    const response = await fetch(serverAddress, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      credentials: "include",
-      body: JSON.stringify(sessionData),
+function uploadData() {
+  const currentSessionId = get(sessionId);
+  console.log("Session Id:", currentSessionId);
+  if (currentSessionId) {
+    console.log("analysisData before updates:", get(analysisData));
+    analysisData.update((data) => {
+      return data.map(item => ({
+        ...item,
+        session_id: currentSessionId,
+      }));
     });
-
-    console.log(sessionData);
-
-    if (!response.ok) {
-      throw new Error("Failed to create session");
-    }
-
-    const result = await response.json();
-    console.log(result.message); // Handle success message
-
-    // Save sessionId to store
-    if (result.sessionId) {
-      sessionId.set(result.sessionId); // Store the sessionId in the writable store
-    }
-
-  } catch (error) {
-    console.error("Error creating session:", error);
+    console.log("analysisData after updates:", get(analysisData));
+    insertAnalysisData(get(analysisData));
+  } else {
+    console.error('Session ID is not available');
   }
 }
->>>>>>> 3b43d016b4aeabb140164bbbc09def8d2b377310
