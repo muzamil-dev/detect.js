5f951137c06af62727ee57d40eca32b162474da5
muzamil-dev <mshamsifl@gmail.com>
2025-03-04 22:02:45 -0500

    Add local (node) and vercel adapters

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,14 @@ import svelte from "@astrojs/svelte";
 
 import preact from "@astrojs/preact";
 
+import vercel from "@astrojs/vercel";
+
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), preact()],
+  adapter: node({
+    mode: "standalone",
+  }),
 });
