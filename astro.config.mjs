d3ca7ef8217816c283cbe250b5f27e24522b8a6c
Muzamil-dev <shamsimuzamil@gmail.com>
2026-07-24 10:29:38 -0400

    fix: static vercel deployment configuration and vercel.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
-
 import preact from "@astrojs/preact";
-
 import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   integrations: [tailwind(), svelte(), preact()],
-  adapter: vercel(),
 });
